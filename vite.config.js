d1c4b75a813eb0373eeee73d8952a1f437a720e3
ClideJagape <kingclidejagape122770@gmail.com>
2025-10-18 11:15:50 +0800

    Update base path for production environment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   root: 'src',                  
   publicDir: '../static',       
   base: process.env.NODE_ENV === 'production'
-    ? '/Jagape-ITE18-Act1.11/' 
+    ? '/Jagape-ITE18-Act1.10/' 
     : '/',
   server: {
     host: true,
